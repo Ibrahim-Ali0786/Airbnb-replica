eed5179ee019206801fba195f899169c255fcccc
Ibrahim Tin <ibrahimtin0786@gmail.com>
2024-06-28 20:38:34 +0530

    CRUD implementation is done with styling

--- models/listing.js
+++ models/listing.js
@@ -13,8 +13,8 @@ const listeningSchema = new  mongoose.Schema ({
      {
       url:{
          type:String,
-         default:"https://unsplash.com/photos/a-flock-of-birds-flying-over-the-ocean-at-sunset-C3wnYmz8fGA",
-         set: (v)=> v==="" ? "https://unsplash.com/photos/a-flock-of-birds-flying-over-the-ocean-at-sunset-C3wnYmz8fGA" : v,
+         default:"https://images.unsplash.com/photo-1566073771259-6a8506099945?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+         set: (v)=> v==="" ? "https://images.unsplash.com/photo-1566073771259-6a8506099945?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
       },
       filename:{
          type:String,
